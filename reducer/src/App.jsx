2af1c0170696bd892e1bcba80e776392b31a0438
king-kanda <stevenokanda@gmail.com>
2025-01-14 17:17:08 +0300

    usereduce

---
+++ reducer/src/App.jsx
@@ -0,0 +1,27 @@
+import { useReducer } from "react";
+
+const App = () => {
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+  const [state , dispatch] = useReducer(reducer , {count : 0});
+  
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
